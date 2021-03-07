27a64556d47fe34f72d8139af429f1c8580a515a
Kuba <main.kubabudzynsk@gmail.com>
2021-03-07 04:12:45 +0100

    admin panel styles and ad placement in a post

--- plugins/AdvertisementForPost/advertisement-for-post.php
+++ plugins/AdvertisementForPost/advertisement-for-post.php
@@ -10,58 +10,83 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html 
  */
 
+
+
 function ad_post_admin_actions_register_menu() {  
-    add_options_page("Advertisement For Post", "Advertisement For Post", 'manage_options', "ad_post", "ad_post_admin_page");    
+    add_menu_page("Manage advertisements", "Ads", 'manage_options', "add_adv", "ad_post_admin_page", "dashicons-welcome-widgets-menus");    
 } 
- 
 add_action('admin_menu', 'ad_post_admin_actions_register_menu'); 
+
+
 
 /* TODO: walidacja (długość), usuwanie ogłoszenia, losowanie i wyświetlanie,  kolejny plugin*/
 
+
+
 function ad_post_admin_page() { 
-    // get _POST variable from globals                                             
+                                          
     global $_POST; 
-    
-    //read current option value
-    $opAdList = [];
     $opAdList  =  is_array(get_option('ad_post_list')) ? get_option('ad_post_list') : [];
                 
+    if(isset($_POST['id_value']) and $_POST['id_value']!=="") {
+        //delete_option('ad_post_list');
+        unset($opAdList[$_POST['id_value']]);
+        update_option('ad_post_list', $opAdList); 
+        echo'<div class="notice notice-error is-dismissible"><p>Deleted advertisment.</p></div>';    
+    } 
 
-    // process new ad
     if(isset($_POST['ad_post_new']) and $_POST['ad_post_new']!=="") { 
         array_push($opAdList, $_POST['ad_post_new']);
         update_option('ad_post_list', $opAdList); 
         echo'<div class="notice notice-success is-dismissible"><p>Added new advertisment.</p></div>';     
-    } 
-     
-    //display admin page
+    }
+
     ?>
-    <div class="wrap">
-        <h1>Advertisement For Post</h1>
-        <?php 
-        if (is_array($opAdList)) {
-            foreach ($opAdList as $value) {
-                echo "<div class='ad_post_ad_admin'>$value</div>";
-            }
-        }
-        ?>
+    <div class="wrap center">
         <form name="ad_post_add_new_form" method="post">
-            <p>New Advertisment:
-                <textarea name="ad_post_new"></textarea>
-            </p>
-            <p class="submit">
-                <input type="submit" value="Add new">
-            </p>
+            <div style="width: 100%;">
+                <div >
+                    <h1 class="title">New Advertisment</h1>
+                    <textarea name="ad_post_new" class="insertText" type="text"></textarea>
+                </div>
+                <p class="submitButton">
+                    <button type="submit" style="margin: 0 auto;">Add new</button>
+                </p>
+            </div>
         </form>
+        <h1 class="title">Advertisement For Post</h1>
+        <div class="spaceAds">
+        <?php foreach ($opAdList as $key=>$value) : ?>
+            <form class='element' method='post' name='delete_post'>
+                <div class='content'>
+                    <?= $value ?>
+                </div>
+                <input type='hidden' id='id_value' name='id_value' value=<?= $key ?> >
+                <button class='deleteButton' type='submit'>DELETE</button>
+            </form>
+        <?php endforeach ?>    
+        </div>
     </div>
     <?php
 }
 
+
+
+function insertBeforeContent($content){
+    if(is_singular() && in_the_loop() && is_main_query()){
+        $opAdList  =  is_array(get_option('ad_post_list')) ? get_option('ad_post_list') : [];
+        $randomElement = $opAdList[array_rand($opAdList, 1)];
+        $custom = "<div class='center' style='padding: 2rem; border: solid gray 2px; width: 100%'>$randomElement</div>";
+        return $custom.$content;
+    }
+    return $content;
+}
+add_filter('the_content', "insertBeforeContent");
+
+
+
 function ad_post_register_styles() { 
-    //register style 
     wp_register_style('ad_post_styles', plugins_url('/css/style.css', __FILE__)); 
-    //enable style (load in meta of html)
     wp_enqueue_style('ad_post_styles'); 
 } 
-
 add_action('init', 'ad_post_register_styles');
